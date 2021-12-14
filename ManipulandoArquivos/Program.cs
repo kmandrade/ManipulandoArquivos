53ad1a7c4919b3974d0f6c369f02c8818fc8c90a
kelvin <kelvin@PROTECH-SSC6>
2021-12-14 11:34:28 -0300

    Add project files.

---
+++ ManipulandoArquivos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ManipulandoArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            var controleDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024];
+
+            
+            var numByteLidos = -1;
+            while (numByteLidos != 0)
+            {
+                numByteLidos = controleDoArquivo.Read(buffer, 0, 1024);
+                EscreveArquivo(buffer);
+            }
+
+
+            Console.ReadLine();
+
+        }
+
+        static void EscreveArquivo(byte[] buffer)
+        {
+            foreach (var MyByte in buffer)
+            {
+                Console.Write(MyByte);
+                Console.Write(" ");
+            }
+        }
+        
+    }
+}
